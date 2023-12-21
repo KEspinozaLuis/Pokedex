62851eede579ebfed4eb8083207a1401432268b8
Keny Espinoza Luis <kespinozaluis@gmail.com>
2023-12-21 00:58:59 -0500

    update ruta image loading

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -3,7 +3,7 @@ import styles from './loading.module.css'
 const Loading = () =>{
     return (
         <div className={styles.loading}>
-            <img src="./pokebola.png" alt="pokebola" />
+            <img src="/pokebola.png" alt="pokebola" />
             <span>Loading...</span>
         </div>
     )
